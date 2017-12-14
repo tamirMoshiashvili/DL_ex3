a9ef46abe68e2c3b40380940348a926905ed5594
Tamir Moshiashvili <norblet82@gmail.com>
2017-12-15 00:50:13 +0200

    # completed part 1.2

--- part1/gen_examples.py
+++ part1/gen_examples.py
@@ -3,6 +3,8 @@ from StringIO import StringIO
 
 MAX_SEQ_SIZE = 25
 SAMPLE_SIZE = 500
+TRAIN_SIZE = 15000
+TEST_SIZE = 15000
 
 
 def generate_line(mode):
@@ -45,7 +47,20 @@ def generate_sample_and_save(mode, filename):
     write_examples_to_file(filename, example_list)
 
 
+def generate_set_and_save(filename, set_size):
+    modes = ['pos', 'neg']
+    lines = []
+    for _ in range(set_size):
+        lines.append(generate_line(random.choice(modes)))
+    # save in file
+    write_examples_to_file(filename, lines)
+
+
 if __name__ == '__main__':
     # generate examples
     generate_sample_and_save('pos', 'pos_examples')
     generate_sample_and_save('neg', 'neg_examples')
+
+    # generate sets
+    generate_set_and_save('train_set', TRAIN_SIZE)
+    generate_set_and_save('test_set', TEST_SIZE)
